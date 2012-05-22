75c274df78c5918c60719389553531b29fe37b67
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-05-22 13:29:20 +0900

    fix yaml mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,6 @@
 ;; init.el
 ;; Edit by Tatsuya Hoshino
-;; 2012-04-10
+;; 2012-05-22
 
 ;; load-path を追加する関数を定義
 (defun add-to-load-path (&rest paths)
@@ -243,5 +243,6 @@
   )
 
 ;; Yaml
-(when (require 'yaml-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
+;(when (require 'yaml-mode nil t)
+;  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
+(require 'yaml-mode)
