d6c5c8c46265f27afd25ba4bcef425b59608a335
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2012-10-07 21:08:56 +0900

    add window move settings to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -40,6 +40,14 @@
 ;; C-o を other-window にする
 (define-key global-map (kbd "C-o") 'other-window)
 
+;; 反対側のウィンドウにいけるように
+(setq windmove-wrap-around t)
+;; C-M-{h,j,k,l}でウィンドウ間を移動
+(define-key global-map (kbd "C-M-k") 'windmove-up)
+(define-key global-map (kbd "C-M-j") 'windmove-down)
+(define-key global-map (kbd "C-M-l") 'windmove-right)
+(define-key global-map (kbd "C-M-h") 'windmove-left)
+
 ;; ターミナル以外の場合
 (when window-system
   ;; tool-bar を非表示
