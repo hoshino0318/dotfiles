769bb2c4a6c6b88c8fc7772b9b55cf46a0d039cc
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2012-11-07 23:36:30 +0900

    add dmacro to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -64,6 +64,11 @@
 (set-face-background 'default "black")
 (setq frame-background-mode 'dark)
 
+;; dmacro
+(defconst *dmacro-key* "\C-t" "繰返し指定キー")
+(global-set-key *dmacro-key* 'dmacro-exec)
+(autoload 'dmacro-exec "dmacro" nil t)
+
 ;; 最大限色付け
 (setq font-lock-maximum-decoration t)
 
