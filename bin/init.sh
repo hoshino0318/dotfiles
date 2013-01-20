a98b9d9557e63135b228f47623a2213e7237a620
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2013-01-21 08:10:01 +0900

    fix bin/init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -1,20 +1,23 @@
-#!/bin/sh
+#!/bin/sh -x
 
 basedir=`dirname $0`/../..
 
-# set files
 FILE_LIST=(.emacs.d .screenrc .vimrc .vim .zshrc)
 
+: [Info] Set synblic link
 for file in ${FILE_LIST[@]}; do
   if [ -f ~/$file -o -d ~/$file ]; then
     rm -rf ~/$file
   fi
   ln -s $HOME/dotfiles/$file ~/
 done
+echo;
 
-# initialize git submodle
+: [Info] Initialize git submodle
 cd $HOME/dotfiles
 git submodule update --init
+echo;
 
-# vim NeoBundleInstall
+: [Info] Vim NeoBundleInstall
 vim -S .vim/neobundleinstall.vim
+echo;
