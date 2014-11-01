0a80bc549140177723ecaba6e45a6b9a0ebd3bce
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2014-11-01 16:29:52 +0900

    Add web-mode to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -445,6 +445,37 @@
 (custom-set-variables
  '(sql-indent-offset 2))
 
+;; web-mode
+;; http://web-mode.org/
+(when (require 'web-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+
+  ;; 色の設定
+  (custom-set-faces
+   '(web-mode-doctype-face
+     ((t (:foreground "#82AE46"))))
+   '(web-mode-html-tag-face
+     ((t (:foreground "#E6B422" :weight bold))))
+   '(web-mode-html-attr-name-face
+     ((t (:foreground "#C97586"))))
+   '(web-mode-html-attr-value-face
+     ((t (:foreground "#82AE46"))))
+   '(web-mode-comment-face
+     ((t (:foreground "#D9333F"))))
+   '(web-mode-server-comment-face
+     ((t (:foreground "#D9333F"))))
+   '(web-mode-css-rule-face
+     ((t (:foreground "#A0D8EF"))))
+   '(web-mode-css-pseudo-class-face
+     ((t (:foreground "#FF7F00"))))
+   '(web-mode-css-at-rule-face
+     ((t (:foreground "#FF7F00")))))
+  )
+
 ;; JavaScript
 (when (require 'js2-mode nil t)
   ; Use default js-mode instead of espresso
