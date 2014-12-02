d37864ec63fc510406f5e3a17bc75724da2b6c78
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2014-12-02 23:18:55 +0900

    Change html tag colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -461,13 +461,13 @@
   ;; 色の設定
   (custom-set-faces
    '(web-mode-doctype-face
-     ((t (:foreground "#82AE46"))))
+     ((t (:foreground "#BF1E56"))))
    '(web-mode-html-tag-face
-     ((t (:foreground "#E6B422" :weight bold))))
+     ((t (:foreground "#DB531F"))))
    '(web-mode-html-attr-name-face
-     ((t (:foreground "#C97586"))))
+     ((t (:foreground "#44A5CB"))))
    '(web-mode-html-attr-value-face
-     ((t (:foreground "#82AE46"))))
+     ((t (:foreground "#D8DAB4"))))
    '(web-mode-comment-face
      ((t (:foreground "#D9333F"))))
    '(web-mode-server-comment-face
