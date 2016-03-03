7ef7fbeb2256dce84eb10d363679374b32b09d56
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2016-03-03 22:40:20 +0900

    Obsolete dont-compile

--- .emacs.d/elisp/init-loader.el
+++ .emacs.d/elisp/init-loader.el
@@ -69,143 +69,142 @@ e.x, 00_hoge.el, 01_huga.el ... 99_keybind.el"
 
 (defcustom init-loader-ntemacs-regexp "^ntemacs-"
   "ntemacs 使用時に読み込まれる設定ファイルにマッチする正規表現"
-    :group 'init-loader
-    :type 'regexp)
+  :group 'init-loader
+  :type 'regexp)
 
-  (defcustom init-loader-carbon-emacs-regexp "^carbon-emacs-"
-    "carbon-emacs 使用時に読み込まれる設定ファイルにマッチする正規表現"
-    :group 'init-loader
-    :type 'regexp)
+(defcustom init-loader-carbon-emacs-regexp "^carbon-emacs-"
+  "carbon-emacs 使用時に読み込まれる設定ファイルにマッチする正規表現"
+  :group 'init-loader
+  :type 'regexp)
 
-  (defcustom init-loader-cocoa-emacs-regexp "^cocoa-emacs-"
-    "cocoa-emacs 使用時に読み込まれる設定ファイルにマッチする正規表現"
-    :group 'init-loader
-    :type 'regexp)
+(defcustom init-loader-cocoa-emacs-regexp "^cocoa-emacs-"
+  "cocoa-emacs 使用時に読み込まれる設定ファイルにマッチする正規表現"
+  :group 'init-loader
+  :type 'regexp)
 
-  (defcustom init-loader-nw-regexp "^nw-"
-    "no-window環境での起動時に読み込まれる設定ファイルにマッチする正規表現"
-    :group 'init-loader
-    :type 'regexp)
+(defcustom init-loader-nw-regexp "^nw-"
+  "no-window環境での起動時に読み込まれる設定ファイルにマッチする正規表現"
+  :group 'init-loader
+  :type 'regexp)
 
 ;;; Code
-  (defun* init-loader-load (&optional (init-dir init-loader-directory))
-    (let ((init-dir (init-loader-follow-symlink init-dir)))
-      (assert (and (stringp init-dir) (file-directory-p init-dir)))
-      (init-loader-re-load init-loader-default-regexp init-dir t)
-      ;; meadow
-      (and (featurep 'meadow)
-           (init-loader-re-load init-loader-meadow-regexp init-dir))
-      (and (eq system-type 'windows-nt)
-           ;;(load "ntemacs-config.el"))
-           (init-loader-re-load init-loader-ntemacs-regexp init-dir))
-      ;; carbon emacs
-      (and (featurep 'carbon-emacs-package)
-           (init-loader-re-load init-loader-carbon-emacs-regexp init-dir))
-      ;; cocoa emacs
-      (and (equal window-system 'ns)
-           (init-loader-re-load init-loader-cocoa-emacs-regexp init-dir))
-      ;; no window
-      (and (null window-system)
-           (init-loader-re-load init-loader-nw-regexp init-dir))
+(defun* init-loader-load (&optional (init-dir init-loader-directory))
+  (let ((init-dir (init-loader-follow-symlink init-dir)))
+    (assert (and (stringp init-dir) (file-directory-p init-dir)))
+    (init-loader-re-load init-loader-default-regexp init-dir t)
+    ;; meadow
+    (and (featurep 'meadow)
+         (init-loader-re-load init-loader-meadow-regexp init-dir))
+    (and (eq system-type 'windows-nt)
+         ;;(load "ntemacs-config.el"))
+         (init-loader-re-load init-loader-ntemacs-regexp init-dir))
+    ;; carbon emacs
+    (and (featurep 'carbon-emacs-package)
+         (init-loader-re-load init-loader-carbon-emacs-regexp init-dir))
+    ;; cocoa emacs
+    (and (equal window-system 'ns)
+         (init-loader-re-load init-loader-cocoa-emacs-regexp init-dir))
+    ;; no window
+    (and (null window-system)
+         (init-loader-re-load init-loader-nw-regexp init-dir))
 
-      (when init-loader-show-log-after-init
-        (add-hook  'after-init-hook 'init-loader-show-log))))
+    (when init-loader-show-log-after-init
+      (add-hook  'after-init-hook 'init-loader-show-log))))
 
-  (defun init-loader-follow-symlink (dir)
-    (cond ((file-symlink-p dir)
-           (expand-file-name (file-symlink-p dir)))
-          (t (expand-file-name dir))))
+(defun init-loader-follow-symlink (dir)
+  (cond ((file-symlink-p dir)
+         (expand-file-name (file-symlink-p dir)))
+        (t (expand-file-name dir))))
 
-  (lexical-let (logs)
-    (defun init-loader-log (&optional s)
-      (if s (and (stringp s) (push s logs)) (mapconcat 'identity (reverse logs) "\n"))))
+(lexical-let (logs)
+  (defun init-loader-log (&optional s)
+    (if s (and (stringp s) (push s logs)) (mapconcat 'identity (reverse logs) "\n"))))
 
-  (lexical-let (err-logs)
-    (defun init-loader-error-log (&optional s)
-      (if s (and (stringp s) (push s err-logs)) (mapconcat 'identity (reverse err-logs) "\n"))))
+(lexical-let (err-logs)
+  (defun init-loader-error-log (&optional s)
+    (if s (and (stringp s) (push s err-logs)) (mapconcat 'identity (reverse err-logs) "\n"))))
 
-  (defun init-loader-re-load (re dir &optional sort)
-    (let ((load-path (cons dir load-path)))
-      (dolist (el (init-loader--re-load-files re dir sort))
-        (condition-case e
-            (let ((time (car (benchmark-run (load (file-name-sans-extension el))))))
-              (init-loader-log (format "loaded %s. %s" (locate-library el) time)))
-          (error
-           (init-loader-error-log (error-message-string e)))))))
+(defun init-loader-re-load (re dir &optional sort)
+  (let ((load-path (cons dir load-path)))
+    (dolist (el (init-loader--re-load-files re dir sort))
+      (condition-case e
+          (let ((time (car (benchmark-run (load (file-name-sans-extension el))))))
+            (init-loader-log (format "loaded %s. %s" (locate-library el) time)))
+        (error
+         (init-loader-error-log (error-message-string e)))))))
 
-  (defun init-loader--re-load-files (re dir &optional sort)
-    (loop for el in (directory-files dir t)
-          when (string-match re (file-name-nondirectory el))
-          collect (file-name-nondirectory el) into ret
-          finally return (if sort (sort ret 'string<) ret)))
+(defun init-loader--re-load-files (re dir &optional sort)
+  (loop for el in (directory-files dir t)
+        when (string-match re (file-name-nondirectory el))
+        collect (file-name-nondirectory el) into ret
+        finally return (if sort (sort ret 'string<) ret)))
 
-  (defun init-loader-show-log ()
-    "return buffer"
-    (interactive)
-    (let ((b (get-buffer-create "*init log*")))
-      (with-current-buffer b
-        (erase-buffer)
-        (insert "------- error log -------\n\n"
-                (init-loader-error-log)
-                "\n\n")
-        (insert "------- init log -------\n\n"
-                (init-loader-log)
-                "\n\n")
-        ;; load-path
-        (insert "------- load path -------\n\n"
-                (mapconcat 'identity load-path "\n"))
-        (goto-char (point-min)))
-      (switch-to-buffer b)))
+(defun init-loader-show-log ()
+  "return buffer"
+  (interactive)
+  (let ((b (get-buffer-create "*init log*")))
+    (with-current-buffer b
+      (erase-buffer)
+      (insert "------- error log -------\n\n"
+              (init-loader-error-log)
+              "\n\n")
+      (insert "------- init log -------\n\n"
+              (init-loader-log)
+              "\n\n")
+      ;; load-path
+      (insert "------- load path -------\n\n"
+              (mapconcat 'identity load-path "\n"))
+      (goto-char (point-min)))
+    (switch-to-buffer b)))
 
 
 ;;;; Test
-  (defvar init-loader-test-files
-    '("00_utils.el"
-      "23_yaml.el"
-      "01_ik-cmd.el"
-      "96_color.el"
-      "20_elisp.el"
-      "21_javascript.el"
-      "25_perl.el"
-      "98_emacs-config.el"
-      "99_global-keys.el"
-      "carbon-emacs-config.el"
-      "carbon-emacs-migemo.el"
-      "nw-config.el"
-      "emacs-migemo.el"
-      "meadow-cmd.el"
-      "meadow-config.el"
-      "meadow-gnuserv.el"
-      "meadow-shell.el"
-      "meadow-w32-symlinks.el"))
+(defvar init-loader-test-files
+  '("00_utils.el"
+    "23_yaml.el"
+    "01_ik-cmd.el"
+    "96_color.el"
+    "20_elisp.el"
+    "21_javascript.el"
+    "25_perl.el"
+    "98_emacs-config.el"
+    "99_global-keys.el"
+    "carbon-emacs-config.el"
+    "carbon-emacs-migemo.el"
+    "nw-config.el"
+    "emacs-migemo.el"
+    "meadow-cmd.el"
+    "meadow-config.el"
+    "meadow-gnuserv.el"
+    "meadow-shell.el"
+    "meadow-w32-symlinks.el"))
 
-  (dont-compile
-    (when (fboundp 'expectations)
-      (expectations 
-       (desc "init-loader--re-load-files")
-       (expect  '("00_utils.el" "01_ik-cmd.el" "20_elisp.el" "21_javascript.el" "23_yaml.el" "25_perl.el" "96_color.el" "98_emacs-config.el" "99_global-keys.el")
-                (stub directory-files => init-loader-test-files)
-                (init-loader--re-load-files init-loader-default-regexp "" t))
-       (expect  '("meadow-cmd.el" "meadow-config.el" "meadow-gnuserv.el" "meadow-shell.el" "meadow-w32-symlinks.el")
-                (stub directory-files => init-loader-test-files)
-                (init-loader--re-load-files init-loader-meadow-regexp "" t))
+(when (fboundp 'expectations)
+  (expectations
+   (desc "init-loader--re-load-files")
+   (expect  '("00_utils.el" "01_ik-cmd.el" "20_elisp.el" "21_javascript.el" "23_yaml.el" "25_perl.el" "96_color.el" "98_emacs-config.el" "99_global-keys.el")
+            (stub directory-files => init-loader-test-files)
+            (init-loader--re-load-files init-loader-default-regexp "" t))
+   (expect  '("meadow-cmd.el" "meadow-config.el" "meadow-gnuserv.el" "meadow-shell.el" "meadow-w32-symlinks.el")
+            (stub directory-files => init-loader-test-files)
+            (init-loader--re-load-files init-loader-meadow-regexp "" t))
 
-       (expect  '("carbon-emacs-config.el" "carbon-emacs-migemo.el")
-                (stub directory-files => init-loader-test-files)
-                (init-loader--re-load-files init-loader-carbon-emacs-regexp "" t))
-       (expect  '("nw-config.el")
-                (stub directory-files => init-loader-test-files)
-                (init-loader--re-load-files init-loader-nw-regexp "" t))
-       ;; 環境依存
-       (desc "follow symlink")
-       (expect "c/.emacs.d/inits"
-               (file-relative-name (file-symlink-p "~/tmp/el-inits"))) ; symlink
-       (desc "init-loader-follow-symlink")
-       (expect "c/.emacs.d/inits"
-               (file-relative-name (init-loader-follow-symlink "~/tmp/el-inits")))
-       (expect "c/.emacs.d/inits"
-               (file-relative-name (init-loader-follow-symlink "~/tmp/el-inits")))
-       )))
+   (expect  '("carbon-emacs-config.el" "carbon-emacs-migemo.el")
+            (stub directory-files => init-loader-test-files)
+            (init-loader--re-load-files init-loader-carbon-emacs-regexp "" t))
+   (expect  '("nw-config.el")
+            (stub directory-files => init-loader-test-files)
+            (init-loader--re-load-files init-loader-nw-regexp "" t))
+   ;; 環境依存
+   (desc "follow symlink")
+   (expect "c/.emacs.d/inits"
+           (file-relative-name (file-symlink-p "~/tmp/el-inits"))) ; symlink
+   (desc "init-loader-follow-symlink")
+   (expect "c/.emacs.d/inits"
+           (file-relative-name (init-loader-follow-symlink "~/tmp/el-inits")))
+   (expect "c/.emacs.d/inits"
+           (file-relative-name (init-loader-follow-symlink "~/tmp/el-inits")))
+   ))
 
-  (provide 'init-loader)
+(provide 'init-loader)
 
